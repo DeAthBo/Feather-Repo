6355293b8962e01fcf65dd77d23bf7806f283d17
DeAthBo <34346682+DeAthBo@users.noreply.github.com>
2025-08-24 04:57:06 +0900

    Update pro_response.js

--- pro_response.js
+++ pro_response.js
@@ -7,10 +7,8 @@ const currentRequestBody = $request.body;
 // 检查请求的 Body 是否包含了第二次请求的独有特征（例如 "NU="）
 if (currentRequestBody && currentRequestBody.includes('NU=')) {
   // 如果特征匹配，就执行响应替换
-  console.log('匹配到第二次请求特征，执行响应修改');
   $done({ body: targetResponseBody });
 } else {
   // 如果不匹配，就不做任何修改，直接放行原始响应
-  console.log('未匹配到特征，放行原始响应');
-  $done({});
+  $done({ body: targetResponseBody });
 }

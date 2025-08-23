db2236574c61d38245993b1ff6bb1103cba3c240
DeAthBo <34346682+DeAthBo@users.noreply.github.com>
2025-08-24 04:58:36 +0900

    Update pro_response.js

--- pro_response.js
+++ pro_response.js
@@ -1,14 +1,2 @@
-// 你想替换成的最终响应内容
-const targetResponseBody = '076d1dab5e271f439fd25ea7383739e26d8d5b9c1575263727df5fa45689efe5de3a514c80577c82a11b0d9e7413f876ddc16bec66c533070c61515446198830';
-
-// 获取当前请求的 Body
-const currentRequestBody = $request.body;
-
-// 检查请求的 Body 是否包含了第二次请求的独有特征（例如 "NU="）
-if (currentRequestBody && currentRequestBody.includes('NU=')) {
-  // 如果特征匹配，就执行响应替换
-  $done({ body: targetResponseBody });
-} else {
-  // 如果不匹配，就不做任何修改，直接放行原始响应
-  $done({ body: targetResponseBody });
-}
+const newBody = '076d1dab5e271f439fd25ea7383739e26d8d5b9c1575263727df5fa45689efe5de3a514c80577c82a11b0d9e7413f876ddc16bec66c533070c61515446198830';
+$done({ body: newBody });
